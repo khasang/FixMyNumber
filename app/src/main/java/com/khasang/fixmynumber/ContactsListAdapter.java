0a5eb7147ed5681bdf32f79f884ad4edcd82ca13
Raenar4k <RaenarDev@gmail.com>
2015-10-16 15:19:56 +0200

    added ContactListAdapter and RecyclerView for contactsList

---
+++ app/src/main/java/com/khasang/fixmynumber/ContactsListAdapter.java
@@ -0,0 +1,64 @@
+package com.khasang.fixmynumber;
+
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.Adapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by Raenar on 16.10.2015.
+ */
+public class ContactsListAdapter extends RecyclerView.Adapter<ContactsListAdapter.ViewHolder> {
+
+    private List<ContactItem> contactsList;
+
+    public ContactsListAdapter(List<ContactItem> contactsList) {
+        this.contactsList = contactsList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.contact_item, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int i) {
+        final ContactItem contact = contactsList.get(i);
+        viewHolder.checkBox.setChecked(contact.isChecked());
+        viewHolder.name.setText(contact.getName());
+        viewHolder.number.setText(contact.getNumberOriginal());
+
+        viewHolder.checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                contact.setIsChecked(isChecked);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return contactsList.size();
+    }
+
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        private CheckBox checkBox;
+        private TextView name;
+        private TextView number;
+
+        public ViewHolder(View v) {
+            super(v);
+            checkBox = (CheckBox) v.findViewById(R.id.checkBox);
+            name = (TextView) v.findViewById(R.id.contactName);
+            number = (TextView) v.findViewById(R.id.contactNumber);
+        }
+    }
+}
--- app/src/main/java/com/khasang/fixmynumber/MainActivity.java
+++ app/src/main/java/com/khasang/fixmynumber/MainActivity.java
@@ -2,12 +2,50 @@ package com.khasang.fixmynumber;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<ContactItem> contactList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createDummyContacts();
+        setUpRecyclerView();
+    }
+
+    private void createDummyContacts() {
+        contactList = new ArrayList<ContactItem>();
+        for (int i = 0; i < 8; i++) {
+            ContactItem newItem = new ContactItem("qwerty", "12345", null, false);
+            contactList.add(newItem);
+        }
+    }
+
+    private void setUpRecyclerView() {
+        RecyclerView RecyclerViewContacts = (RecyclerView) findViewById(R.id.recyclerViewContacts);
+        ContactsListAdapter adapter = new ContactsListAdapter(contactList);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        RecyclerViewContacts.setAdapter(adapter);
+        RecyclerViewContacts.setLayoutManager(layoutManager);
+    }
+
+    public void buttonCheck(View view) {
+        TextView textView = (TextView) findViewById(R.id.textView);
+        if (contactList.get(1).isChecked()) {
+            textView.setText("2nd contact is checked");
+        } else {
+            textView.setText("2nd contact is NOT checked");
+        }
     }
 }
