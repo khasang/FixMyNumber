d9f33b4d874aced243f7f636480f065cc2b1604c
Raenar4k <RaenarDev@gmail.com>
2015-10-26 18:46:13 +0200

    added "OnItemClick" listener for RecycleViewSavedContacts

--- app/src/main/java/com/khasang/fixmynumber/Activity/RestoreContactsActivity.java
+++ app/src/main/java/com/khasang/fixmynumber/Activity/RestoreContactsActivity.java
@@ -2,14 +2,70 @@ package com.khasang.fixmynumber.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.khasang.fixmynumber.Adapter.SavedContactsAdapter;
 import com.khasang.fixmynumber.R;
 
-public class RestoreContactsActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class RestoreContactsActivity extends AppCompatActivity implements SavedContactsAdapter.SavedContactsItemClickListener {
+    ArrayList<String> savedContactsList;
+    String selectedTable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restore_contacts);
+
+        selectedTable = null;
+        getDummySavedContacts();
+        setUpRecyclerView();
+        setUpButtons();
+    }
+
+    private void getDummySavedContacts() {
+        savedContactsList = new ArrayList<String>();
+        for (int i = 0; i < 20 ; i++) {
+            savedContactsList.add("Контакты " + i);
+        }
+    }
+
+    private void setUpRecyclerView() {
+        RecyclerView recyclerViewSavedContacts = (RecyclerView) findViewById(R.id.recyclerViewSavedContacts);
+        SavedContactsAdapter savedContactsAdapter = new SavedContactsAdapter(savedContactsList, this);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerViewSavedContacts.setAdapter(savedContactsAdapter);
+        recyclerViewSavedContacts.setLayoutManager(layoutManager);
+    }
+
+    private void setUpButtons() {
+        Button buttonLoad = (Button) findViewById(R.id.buttonLoad);
+        Button buttonDelete = (Button) findViewById(R.id.buttonDelete);
+        buttonLoad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (selectedTable!=null){
+                    Toast.makeText(getApplicationContext(), "Loading "+selectedTable, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        buttonDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (selectedTable!=null){
+                    Toast.makeText(getApplicationContext(), "Deleting "+selectedTable, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onSavedContactsItemClick(String name) {
+        selectedTable = name;
     }
 }
