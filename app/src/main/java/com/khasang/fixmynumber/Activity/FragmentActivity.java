43264a7cb991f5cf9191fc69fd66cf8c6ee36d44
Raenar4k <RaenarDev@gmail.com>
2015-11-07 13:14:55 +0200

    disabled change 7 to 00, 8 to 0

--- app/src/main/java/com/khasang/fixmynumber/Activity/FragmentActivity.java
+++ app/src/main/java/com/khasang/fixmynumber/Activity/FragmentActivity.java
@@ -174,12 +174,12 @@ public class FragmentActivity extends AppCompatActivity implements StepFragment.
                 case R.id.radioButtonSwap87:
                     swapPrefix("8", "+7");
                     break;
-                case R.id.radioButtonSwap700:
-                    swapPrefix("+7", "00");
-                    break;
-                case R.id.radioButtonSwap80:
-                    swapPrefix("8", "0");
-                    break;
+//                case R.id.radioButtonSwap700:
+//                    swapPrefix("+7", "00");
+//                    break;
+//                case R.id.radioButtonSwap80:
+//                    swapPrefix("8", "0");
+//                    break;
                 case R.id.radioButtonSwapCustom:
                     String s1 = editTextS1.getText().toString();
                     String s2 = editTextS2.getText().toString();
