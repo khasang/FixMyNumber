d64f7562584ca75747323391bed7abf0be106e92
IvanSvirin <ivan_svirin@mail.ru>
2015-11-24 22:25:23 +0600

    fixed method onlyDigits

--- app/src/main/java/com/khasang/fixmynumber/Task/Util.java
+++ app/src/main/java/com/khasang/fixmynumber/Task/Util.java
@@ -6,7 +6,8 @@ package com.khasang.fixmynumber.Task;
 public class Util {
     public static String onlyDigits(String testNumber) {
         for (int i = 0; i < testNumber.length(); i++) {
-            if ((testNumber.charAt(i) == ' ') || (testNumber.charAt(i) == '-')) {
+            if ((testNumber.charAt(i) == ' ') || (testNumber.charAt(i) == '-') ||
+                    (testNumber.charAt(i) == '(') || (testNumber.charAt(i) == ')')) {
                 testNumber = testNumber.substring(0, i) + testNumber.substring(i + 1);
             }
         }
