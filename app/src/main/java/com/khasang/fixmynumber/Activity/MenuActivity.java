2b17fe4589bad2074f1a846ded46915a4e23a8ca
Raenar4k <RaenarDev@gmail.com>
2015-12-23 19:45:49 +0200

    added DuplicatesActivity

--- app/src/main/java/com/khasang/fixmynumber/Activity/MenuActivity.java
+++ app/src/main/java/com/khasang/fixmynumber/Activity/MenuActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.khasang.fixmynumber.R;
 
@@ -15,6 +16,7 @@ public class MenuActivity extends AppCompatActivity {
 
     private static final int PERMISSIONS_CONTACTS_CHANGE_CODE = 0;
     private static final int PERMISSIONS_CONTACTS_RESTORE_CODE = 1;
+    public static final int PERMISSIONS_CONTACTS_DUPLICATES_CODE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,6 +57,22 @@ public class MenuActivity extends AppCompatActivity {
                 }
             }
         });
+        Button buttonDuplicates = (Button) findViewById(R.id.buttonStartDuplicates);
+        buttonDuplicates.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+                        checkSelfPermission(Manifest.permission.WRITE_CONTACTS) != PackageManager.PERMISSION_GRANTED &&
+                        checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+
+                    requestPermissions(new String[]{Manifest.permission.WRITE_CONTACTS, Manifest.permission.READ_CONTACTS},
+                            PERMISSIONS_CONTACTS_DUPLICATES_CODE);
+                } else {
+                    Intent intent = new Intent(getApplicationContext(), DuplicatesActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 
     @Override
@@ -67,6 +85,11 @@ public class MenuActivity extends AppCompatActivity {
         if (requestCode == PERMISSIONS_CONTACTS_RESTORE_CODE
                 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             Intent intent = new Intent(getApplicationContext(), RestoreContactsActivity.class);
+            startActivity(intent);
+        }
+        if (requestCode == PERMISSIONS_CONTACTS_DUPLICATES_CODE
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            Intent intent = new Intent(getApplicationContext(), DuplicatesActivity.class);
             startActivity(intent);
         }
     }
